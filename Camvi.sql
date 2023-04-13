290a47a274e73713681bf68b5f169f138fcd2a31
Víctor López <vicrenlopez14115@gmail.com>
2023-04-13 15:39:01 -0600

    Cambios

--- Camvi.sql
+++ Camvi.sql
@@ -1,52 +1,41 @@
-CREATE TABLE tbTipoUsuario(
-idTipoUsuario INT PRIMARY KEY IDENTITY(1,1),
-tipoUsuario VARCHAR(20)
+CREATE DATABASE Camvi;
+USE Camvi;
+
+CREATE TABLE tbTipoUsuario
+(
+    idTipoUsuario INT PRIMARY KEY IDENTITY (1,1),
+    tipoUsuario   VARCHAR(20)
 );
 
+INSERT INTO tbTipoUsuario (tipoUsuario)
+VALUES ('Administrador'),
+       ('Fotografo'),
+       ('Cliente');
 
-CREATE TABLE tbUsuarios(
-idUsuario INT PRIMARY KEY IDENTITY(1,1),
-usuario VARCHAR(10),
-contrasenia VARCHAR(1O),
-tipoUsuario INT
+
+CREATE TABLE tbUsuarios
+(
+    idUsuario   INT PRIMARY KEY IDENTITY (1,1),
+    correo      VARCHAR(255),
+    nombre      VARCHAR(50) NOT NULL,
+    contacto    VARCHAR(15),7
+    imagen      IMAGE,
+    dui         VARCHAR(10),
+    pass        VARCHAR(25),
+    tipoUsuario INT
 );
 
-
-CREATE TABLE tbFotografos(
-idFotografo INT PRIMARY KEY IDENTITY(1,1),
-nombreFotografo VARCHAR(50),
-contactoFotografo VARCHAR(15),
-duiFotografo VARCHAR(10),
-correoFotografo VARCHAR(30),
-imagenFotografo IMAGE,
-idUsuario INT
-);
+INSERT INTO tbUsuarios (correo, nombre, contacto, imagen, dui, pass, tipoUsuario)
+VALUES ('20190189@gmail.com', 'Víctor', '7010-2904', NULL, 'No tengo', '1234', 1);
 
 
-CREATE TABLE tbClientes(
-idCliente INT PRIMARY KEY IDENTITY(1,1),
-nombreCliente VARCHAR(50),
-contactoCliente VARCHAR(15),
-duiCliente CARCHAR(10),
-correo VARCHAR(30)
-idUsuario INT
-);
-
-
-CREATE TABLE tbSesiones(
-idSesion INT PRIMARY KEY IDENTITY(1,1),
-direccionEvento VARCHAR(200),
-fechaEvento DATE,
-horaInicio TIME,
-horaFinalizacion TIME,
-idFotografo INT,
-idCliente INT
-);
-
-
--------------------------------------------
-
-CREATE TABLE tbAdministrador(
-idAdministrador INT PRIMARY KEY IDENTITY(1,1),
-
+CREATE TABLE tbSesiones
+(
+    idSesion         INT PRIMARY KEY IDENTITY (1,1),
+    direccionEvento  VARCHAR(200),
+    fechaEvento      DATE,
+    horaInicio       TIME,
+    horaFinalizacion TIME,
+    idFotografo      INT,
+    idCliente        INT
 );
