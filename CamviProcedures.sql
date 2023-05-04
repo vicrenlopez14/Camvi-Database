1b3bf49c74d1a6e7a0d4dca86ea8fdeb956bba56
Víctor López <vicrenlopez14115@gmail.com>
2023-05-04 10:47:04 -0600

    Create administrators and camarographers procedures

--- CamviProcedures.sql
+++ CamviProcedures.sql
@@ -1,13 +1,69 @@
 USE Camvi
 
-ALTER PROCEDURE spRegistrarCliente @nombre NVARCHAR(50),
+CREATE PROCEDURE spCrearAdministrador @nombre NVARCHAR(50),
+                                      @correo VARCHAR(255),
+                                      @pass VARCHAR(25),
+                                      @contacto VARCHAR(15),
+                                      @dui VARCHAR(10)
+AS
+BEGIN
+    DECLARE @result TABLE
+                    (
+                        Result INT
+                    );
+
+    BEGIN TRY
+        INSERT INTO tbUsuarios(nombre, correo, pass, contacto, dui, tipoUsuario)
+        VALUES (@nombre, @correo, @pass, @contacto, @dui, 1);
+        INSERT INTO @result (Result) VALUES (1);
+    END TRY
+    BEGIN CATCH
+        INSERT INTO @result (Result) VALUES (0);
+    END CATCH
+
+    SELECT Result FROM @result;
+END
+GO
+
+CREATE PROCEDURE spCrearCamarografo @nombre NVARCHAR(50),
                                     @correo VARCHAR(255),
                                     @pass VARCHAR(25),
                                     @contacto VARCHAR(15),
                                     @dui VARCHAR(10)
 AS
 BEGIN
-    DECLARE @result TABLE (Result INT);
+    DECLARE @result TABLE
+                    (
+                        Result INT
+                    );
+
+    BEGIN TRY
+        INSERT INTO tbUsuarios(nombre, correo, pass, contacto, dui, tipoUsuario)
+        VALUES (@nombre, @correo, @pass, @contacto, @dui, 2);
+        INSERT INTO @result (Result) VALUES (1);
+    END TRY
+    BEGIN CATCH
+        INSERT INTO @result (Result) VALUES (0);
+    END CATCH
+
+    SELECT Result FROM @result;
+END
+GO
+
+EXEC spCrearAdministrador 'Camarografo', 'camarografo@camvi.com', '123', '123', '123'
+
+
+CREATE PROCEDURE spRegistrarCliente @nombre NVARCHAR(50),
+                                    @correo VARCHAR(255),
+                                    @pass VARCHAR(25),
+                                    @contacto VARCHAR(15),
+                                    @dui VARCHAR(10)
+AS
+BEGIN
+    DECLARE @result TABLE
+                    (
+                        Result INT
+                    );
 
     BEGIN TRY
         INSERT INTO tbUsuarios(nombre, correo, pass, contacto, dui, tipoUsuario)
@@ -24,13 +80,12 @@ GO
 
 EXEC spRegistrarCliente '?', '?a', '123', '123', '123'
 
-CREATE PROCEDURE spCodigoRecuperacion
-	@idUsuario INT
+CREATE PROCEDURE spCodigoRecuperacion @idUsuario INT
 AS
 BEGIN
-	DECLARE @codigo INT;
-	SET @codigo = (SELECT ROUND(((9999 - 1000) * RAND() + 1), 0))
-	INSERT INTO tbCodigosRecuperacion VALUES(@codigo,@idUsuario)
+    DECLARE @codigo INT;
+    SET @codigo = (SELECT ROUND(((9999 - 1000) * RAND() + 1), 0))
+    INSERT INTO tbCodigosRecuperacion VALUES (@codigo, @idUsuario)
 END
 GO
 EXEC spCodigoRecuperacion 2
